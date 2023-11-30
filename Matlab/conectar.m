ff9014e303c52f1a14822d441052b212e1b81366
Raúl López Llana <51479560+llanahp@users.noreply.github.com>
2023-11-30 13:45:07 +0100

    vfh

--- Matlab/conectar.m
+++ Matlab/conectar.m
@@ -1,16 +1,4 @@
 % IP_VIRTUAL_MACHINE , NodeHost , IP_LOCAL_MACHINE
 
 %Clase
-rosinit('http://172.29.30.178:11311' , 'NodeHost' , '172.29.29.110');
-
-%Casa
-% rosinit('http://192.168.0.30:11311' , 'NodeHost' , '192.168.0.16');
-
-%Eduroam
-% rosinit('http://172.22.56.114:11311' , 'NodeHost' , '172.22.9.223');
-
-% Robot real
-% rosinit('http://172.29.30.172:11311' , 'NodeHost' , '172.29.29.53');
-
-%Compartiendo datos
-%rosinit('http://192.168.111.221:11311' , 'NodeHost' , '192.168.111.102');
+rosinit('http://172.29.29.94:11311' , 'NodeHost' , '172.29.29.87');
